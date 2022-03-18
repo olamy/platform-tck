33cfc562d9f8ced67a72c27e4ba2a65f5805321f
Olivier Lamy <olamy@apache.org>
2022-03-18 17:08:35 +1000

    more migration
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- servlet/src/main/java/com/sun/ts/tests/servlet/api/jakarta_servlet_http/httpservletrequestwrapper/URLClient.java
+++ servlet/src/main/java/com/sun/ts/tests/servlet/api/jakarta_servlet_http/httpservletrequestwrapper/URLClient.java
@@ -24,34 +24,28 @@ import java.io.PrintWriter;
 
 import com.sun.javatest.Status;
 import com.sun.ts.tests.servlet.common.request.HttpRequestClient;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class URLClient extends HttpRequestClient {
 
-  private static final String CONTEXT_ROOT = "/servlet_jsh_HSReqWrapper_web";
-
-  /**
-   * Entry point for different-VM execution. It should delegate to method
-   * run(String[], PrintWriter, PrintWriter), and this method should not contain
-   * any test configuration.
-   */
-  public static void main(String[] args) {
-    URLClient theTests = new URLClient();
-    Status s = theTests.run(args, new PrintWriter(System.out),
-        new PrintWriter(System.err));
-    s.exit();
-  }
-
-  /**
-   * Entry point for same-VM execution. In different-VM execution, the main
-   * method delegates to this method.
-   */
-  public Status run(String args[], PrintWriter out, PrintWriter err) {
-
+  @BeforeEach
+  public void setupServletName() throws Exception {
     setServletName("TestServlet");
-    setContextRoot(CONTEXT_ROOT);
-
-    return super.run(args, out, err);
   }
+
+  /**
+   * Deployment for the test
+   */
+  @Deployment(testable = false)
+  public static WebArchive getTestArchive() throws Exception {
+    return ShrinkWrap.create(WebArchive.class, "client-test.war")
+            .setWebXML(URLClient.class.getResource("servlet_jsh_HSReqWrapper_web.xml"));
+  }
+
 
   /*
    * @class.setup_props: webServerHost; webServerPort; ts_home;
@@ -315,6 +309,7 @@ public class URLClient extends HttpRequestClient {
    * @test_Strategy: Validate an IllegalArgumentException is thrown is a null
    * request is passed to the Wrapper's constructor.
    */
+  @Test
   public void httpRequestWrapperConstructorTest() throws Exception {
     TEST_PROPS.setProperty(APITEST, "httpRequestWrapperConstructorTest");
     invoke();
@@ -328,6 +323,7 @@ public class URLClient extends HttpRequestClient {
    * @test_Strategy: Validate an IllegalArgumentException is thrown is a null
    * request is passed to the Wrapper's constructor.
    */
+  @Test
   public void httpRequestWrapperConstructorIllegalArgumentExceptionTest()
       throws Exception {
     TEST_PROPS.setProperty(APITEST,
@@ -591,6 +587,7 @@ public class URLClient extends HttpRequestClient {
    * 
    * @test_Strategy: Servlet gets wrapped response object
    */
+  @Test
   public void httpRequestWrapperGetRequestTest() throws Exception {
     TEST_PROPS.setProperty(APITEST, "httpRequestWrapperGetRequestTest");
     invoke();
@@ -603,6 +600,7 @@ public class URLClient extends HttpRequestClient {
    * 
    * @test_Strategy: Servlet sets wrapped response object
    */
+  @Test
   public void httpRequestWrapperSetRequestTest() throws Exception {
     TEST_PROPS.setProperty(APITEST, "httpRequestWrapperSetRequestTest");
     invoke();
@@ -615,6 +613,7 @@ public class URLClient extends HttpRequestClient {
    * 
    * @test_Strategy: Servlet sets wrapped response object
    */
+  @Test
   public void httpRequestWrapperSetRequestIllegalArgumentExceptionTest()
       throws Exception {
     TEST_PROPS.setProperty(APITEST,
