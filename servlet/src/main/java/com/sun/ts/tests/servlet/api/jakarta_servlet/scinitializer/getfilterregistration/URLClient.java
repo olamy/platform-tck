2ce56d826be1c2fa7a0dc9a661f45d5c8fa6eb92
Olivier Lamy <olamy@apache.org>
2022-03-24 22:04:10 +1000

    fix content initializer
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- servlet/src/main/java/com/sun/ts/tests/servlet/api/jakarta_servlet/scinitializer/getfilterregistration/URLClient.java
+++ servlet/src/main/java/com/sun/ts/tests/servlet/api/jakarta_servlet/scinitializer/getfilterregistration/URLClient.java
@@ -20,11 +20,18 @@
 package com.sun.ts.tests.servlet.api.jakarta_servlet.scinitializer.getfilterregistration;
 
 import com.sun.ts.tests.servlet.common.client.AbstractUrlClient;
+import com.sun.ts.tests.servlet.common.util.ResourcesUtils;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
 
 public class URLClient extends AbstractUrlClient {
 
@@ -38,8 +45,12 @@ public class URLClient extends AbstractUrlClient {
    */
   @Deployment(testable = false)
   public static WebArchive getTestArchive() throws Exception {
-    return ShrinkWrap.create(WebArchive.class, "client-test.war")
+    Path tmpJar = ResourcesUtils.createTempJarWihOneEntry("META-INF/services/jakarta.servlet.ServletContainerInitializer",
+            URLClient.class.getResource("jakarta.servlet.ServletContainerInitializer"));
+    WebArchive webArchive = ShrinkWrap.create(WebArchive.class, "client-test.war")
+            .addAsLibraries(tmpJar.toFile())
             .setWebXML(URLClient.class.getResource("servlet_sci_getfilterregistration_web.xml"));
+    return webArchive;
   }
 
   /*
