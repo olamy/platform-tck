b5cac1a125fd34d39ca41c5fc7a3a952f09f1322
Olivier Lamy <olamy@apache.org>
2022-03-27 14:42:56 +1000

    fix archive
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- servlet/src/main/java/com/sun/ts/tests/servlet/pluggability/api/jakarta_servlet/servletresponsewrapper30/URLClient.java
+++ servlet/src/main/java/com/sun/ts/tests/servlet/pluggability/api/jakarta_servlet/servletresponsewrapper30/URLClient.java
@@ -20,8 +20,11 @@
 package com.sun.ts.tests.servlet.pluggability.api.jakarta_servlet.servletresponsewrapper30;
 
 import com.sun.ts.tests.servlet.common.client.AbstractUrlClient;
+import com.sun.ts.tests.servlet.pluggability.common.RequestListener1;
+import com.sun.ts.tests.servlet.pluggability.common.TestServlet1;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.Test;
 
@@ -34,8 +37,12 @@ public class URLClient extends AbstractUrlClient {
    */
   @Deployment(testable = false)
   public static WebArchive getTestArchive() throws Exception {
+    JavaArchive javaArchive = ShrinkWrap.create(JavaArchive.class, "fragment-1.jar")
+            .addClasses(TestServlet1.class, RequestListener1.class)
+            .addAsResource(URLClient.class.getResource("servlet_plu_servletresponsewrapper30_web-fragment.xml"),
+                    "META-INF/web-fragment.xml");
     return ShrinkWrap.create(WebArchive.class, "client-test.war")
-            .setWebXML(URLClient.class.getResource("servlet_plu_servletresponsewrapper30_web.xml"));
+            .addAsLibraries(javaArchive);
   }
 
 
