8763dfd9fe70b815e1b4538e3a27c4e94adffcba
Olivier Lamy <olamy@apache.org>
2022-03-29 08:43:10 +1000

    fix some archive names
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- servlet/src/main/java/com/sun/ts/tests/servlet/pluggability/api/jakarta_servlet/servlet/URLClient.java
+++ servlet/src/main/java/com/sun/ts/tests/servlet/pluggability/api/jakarta_servlet/servlet/URLClient.java
@@ -39,7 +39,7 @@ public class URLClient extends AbstractUrlClient {
             .addClasses(TestServlet1.class, RequestListener1.class)
             .addAsResource(URLClient.class.getResource("servlet_plu_servlet_web-fragment.xml"),
                     "META-INF/web-fragment.xml");
-    return ShrinkWrap.create(WebArchive.class, "servlet_plu_sclistener_web.war")
+    return ShrinkWrap.create(WebArchive.class, "servlet_plu_servlet_web.war")
             .addAsLibraries(javaArchive);
   }
 
